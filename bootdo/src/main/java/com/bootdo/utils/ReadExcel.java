c6f7feb846efa8b4f8cca9e22f92238769c8f634
19002.jv <631364852@qq.com>
2020-12-02 20:40:57 +0800

    read excel

--- bootdo/src/main/java/com/bootdo/utils/ReadExcel.java
+++ bootdo/src/main/java/com/bootdo/utils/ReadExcel.java
@@ -1,43 +1,215 @@
 package com.bootdo.utils;
 
-import jxl.Cell;
-import jxl.Sheet;
-import jxl.Workbook;
-import jxl.read.biff.BiffException;
+import com.bootdo.BootdoApplication;
+import com.bootdo.futures.dao.FuturesDao;
+import com.bootdo.futures.domain.FuturesDO;
+import com.bootdo.futures.service.FuturesService;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
-public class ReadExcel {
+import com.csvreader.CsvReader;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.apache.poi.ss.usermodel.CellType.NUMERIC;
+import static org.apache.poi.ss.usermodel.CellType.STRING;
+
+@Component
+public class ReadExcel implements ApplicationContextAware {
+
+    @Autowired
+    private static FuturesDao futuresService;
+
+    public static ApplicationContext context = null;
+    private static FuturesDO futures = new FuturesDO();
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+    private static SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.futuresService = applicationContext.getBean(FuturesDao.class);
+    }
 
     public static void main(String[] args) {
-        File file = new File("D:\\Program Files\\交易\\tdx\\T0002\\export");
-        try {
-            jxl.Workbook wb = null;
-            InputStream is = new FileInputStream("D:\\Program Files\\交易\\tdx\\T0002\\export\\xx.xls");
-            wb = Workbook.getWorkbook(is);
+        SpringApplication.run(BootdoApplication.class, args);
 
-            int sheetSize = wb.getNumberOfSheets();
-            Sheet sheet = wb.getSheet(0);
-            int row_total = sheet.getRows();
-            for (int j = 0; j < row_total; j++) {
-                if (j <= 5) {
-                    Cell[] cells = sheet.getRow(j);
-                    for (int c = 0; c < cells.length; c++) {
-                        System.out.printf(cells[c].getContents() + "|");
-                    }
-                    System.out.println();
+        File file = new File("D:\\Program Files\\交易\\数据\\豆粕");
+        File[] tempList = file.listFiles();
+
+        if (file.isDirectory()) {
+            for (int i = 0; i < tempList.length; i++) {
+                String extString = tempList[i].toString().substring(tempList[i].toString().lastIndexOf("."));
+                System.out.println(tempList[i].toString());
+                if (".csv".equals(extString)) {
+                    readCsvFile(tempList[i].toString());
+                } else if (".xlsx".equals(extString) || ".xls".equals(extString)) {
+                    readExcel(tempList[i].toString());
                 }
             }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
+        }
+//        readCsvFile("D:\\Program Files\\交易\\数据\\豆粕\\2018011113522038255.csv");
+//        String filePath = "D:\\Program Files\\交易\\数据\\豆粕\\2020030415214926535.xlsx";
+//        Workbook wb = null;
+//        wb = readExcel(filePath);
+
+    }
+
+
+    //读取excel
+    public static Workbook readExcel(String filePath) {
+        Workbook wb = null;
+        if (filePath == null) {
+            return null;
+        }
+        String extString = filePath.substring(filePath.lastIndexOf("."));
+        InputStream is = null;
+        try {
+            is = new FileInputStream(filePath);
+            if (".xls".equals(extString)) {
+                wb = new HSSFWorkbook(is);
+            } else if (".xlsx".equals(extString)) {
+                wb = new XSSFWorkbook(is);
+            } else {
+                wb = null;
+            }
+
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (BiffException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
+        Sheet sheet = wb.getSheetAt(0);
+        int row_total = sheet.getPhysicalNumberOfRows();
+        int tranCodeNumber = 0;
+        for (int j = 0; j < row_total; j++) {
+            if (j <= 20000) {
+                Row row = sheet.getRow(j);
+                if (tranCodeNumber != 0) {
+                    try {
+                        futures.setTrancode(row.getCell(tranCodeNumber).getRichStringCellValue().getString());
+                        futures.setTime(simpleDateFormat.parse(row.getCell(tranCodeNumber + 1).getRichStringCellValue().getString()));
+                        futures.setOpening((float) row.getCell(tranCodeNumber + 4).getNumericCellValue());
+                        futures.setHighest((float) row.getCell(tranCodeNumber + 5).getNumericCellValue());
+                        futures.setLowest((float) row.getCell(tranCodeNumber + 6).getNumericCellValue());
+                        futures.setClosing((float) row.getCell(tranCodeNumber + 7).getNumericCellValue());
+                        futures.setVolume((int) row.getCell(tranCodeNumber + 11).getNumericCellValue());
+                        futures.setVolVolume((int) row.getCell(tranCodeNumber + 13).getNumericCellValue());
+                        futuresService.save(futures);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (tranCodeNumber == 0) {
+                    for (int c = tranCodeNumber; c < row.getPhysicalNumberOfCells(); c++) {
+
+                        if (row.getCell(c).getCellTypeEnum() == STRING && row.getCell(c).getRichStringCellValue().getString().equals("合约")) {
+                            tranCodeNumber = c;
+                        }
+                    }
+                }
+
+                System.out.println("");
+            }
+        }
+
+        return wb;
     }
+
+
+    public static void readCsvFile(String filePath) {
+        try {
+            ArrayList<String[]> csvList = new ArrayList<String[]>();
+            CsvReader reader = new CsvReader(filePath, ',', Charset.forName("GBK"));
+//          reader.readHeaders(); //跳过表头,不跳可以注释掉
+
+            while (reader.readRecord()) {
+                csvList.add(reader.getValues()); //按行读取，并把每一行的数据添加到list集合
+            }
+            reader.close();
+            System.out.println("读取的行数：" + csvList.size());
+            int tranCodeNumber = 0;
+            for (int i = 0; i < csvList.size(); i++) {
+                if (i <= 20000) {
+                    if (tranCodeNumber != 0) {
+                        try {
+                            futures.setTrancode(csvList.get(i)[tranCodeNumber]);
+                            futures.setTime(simpleDateFormat.parse(csvList.get(i)[tranCodeNumber + 1]));
+                            futures.setOpening(Float.parseFloat(csvList.get(i)[tranCodeNumber + 4]));
+                            futures.setHighest(Float.parseFloat(csvList.get(i)[tranCodeNumber + 5]));
+                            futures.setLowest(Float.parseFloat(csvList.get(i)[tranCodeNumber + 6]));
+                            futures.setClosing(Float.parseFloat(csvList.get(i)[tranCodeNumber + 7]));
+                            futures.setVolume(Integer.parseInt(csvList.get(i)[tranCodeNumber + 11]));
+                            futures.setVolVolume(Integer.parseInt(csvList.get(i)[tranCodeNumber + 13]));
+                            futuresService.save(futures);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (tranCodeNumber == 0) {
+                        for (int c = tranCodeNumber; c < csvList.get(i).length; c++) {
+                            System.out.printf(csvList.get(i)[c]);
+                            if (csvList.get(i)[c].equals("合约")) {
+                                tranCodeNumber = c;
+                            }
+                        }
+                    }
+
+                    System.out.println("");
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+//    public static Object getCellFormatValue(Cell cell){
+//        Object cellValue = null;
+//        if(cell!=null){
+//            //判断cell类型
+//            switch(cell.getCellTypeEnum()){
+//                case  NUMERIC:{
+//                    cellValue = String.valueOf(cell.getNumericCellValue());
+//                    break;
+//                }
+//                case FORMULA:{
+//                    //判断cell是否为日期格式
+//                    if(DateUtil.isCellDateFormatted(cell)){
+//                        //转换为日期格式YYYY-mm-dd
+//                        cellValue = cell.getDateCellValue();
+//                    }else{
+//                        //数字
+//                        cellValue = String.valueOf(cell.getNumericCellValue());
+//                    }
+//                    break;
+//                }
+//                case STRING:{
+//                    cellValue = cell.getRichStringCellValue().getString();
+//                    break;
+//                }
+//                default:
+//                    cellValue = "";
+//            }
+//        }else{
+//            cellValue = "";
+//        }
+//        return cellValue;
+//    }
+
 }
---
+++ bootdo/src/main/java/com/bootdo/utils/ReadExcelRM.java
@@ -0,0 +1,170 @@
+package com.bootdo.utils;
+
+import com.bootdo.BootdoApplication;
+import com.bootdo.futures.dao.FuturesDao;
+import com.bootdo.futures.domain.FuturesDO;
+import com.csvreader.CsvReader;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.poi.ss.usermodel.CellType.STRING;
+
+@Component
+public class ReadExcelRM implements ApplicationContextAware {
+
+    @Autowired
+    private static FuturesDao futuresService;
+
+    public static ApplicationContext context = null;
+    private static FuturesDO futures = new FuturesDO();
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+    private static SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.futuresService = applicationContext.getBean(FuturesDao.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootdoApplication.class, args);
+
+        List<String> files = new ArrayList<String>();
+        File file = new File("D:\\Program Files\\交易\\数据");
+        File[] tempList = file.listFiles();
+
+//        if(file.isDirectory()){
+//            for (int i = 0; i < tempList.length ; i++) {
+//                String extString = tempList[i].toString().substring(tempList[i].toString().lastIndexOf("."));
+//                System.out.println(tempList[i].toString());
+//                files.add(tempList[i].toString());
+//               if (".xlsx".equals(extString) || ".xls".equals(extString)){
+//                    readExcel(tempList[i].toString());
+//                }else if (".txt".equals(extString)){
+//                    readTxt(tempList[i].toString());
+//                }
+//            }
+//        }
+
+
+        readTxt("D:\\Program Files\\交易\\数据\\datahistory2010.txt");
+//        readExcel("D:\\Program Files\\交易\\数据\\RM2015.xls");
+    }
+
+    private static void readTxt(String filePath) {
+        try {
+//            FileReader fileReader = new FileReader(filePath);
+            InputStreamReader fileReader = new InputStreamReader(new FileInputStream(filePath), "GBK");
+            BufferedReader buf = new BufferedReader(fileReader);
+
+            int i = 0;
+            int tranCodeNumber = 0;
+            String readLine = "";
+            String[] trans = new String[20];
+            while ((readLine = buf.readLine()) != null) {
+                if (i < 3) {
+                    System.out.println(readLine);
+                    trans = readLine.split("\\\t\\|");
+
+                    if (tranCodeNumber != 0) {
+                        try {
+                            futures.setTime(simpleDateFormat.parse(trans[tranCodeNumber - 1].split("\\|")[0].replaceAll("-", "")));
+                            futures.setTrancode(trans[tranCodeNumber - 1].split("\\|")[1]);
+                            futures.setOpening(Float.parseFloat(trans[tranCodeNumber + 1].replaceAll(",", "")));
+                            futures.setHighest(Float.parseFloat(trans[tranCodeNumber + 2].replaceAll(",", "")));
+                            futures.setLowest(Float.parseFloat(trans[tranCodeNumber + 3].replaceAll(",", "")));
+                            futures.setClosing(Float.parseFloat(trans[tranCodeNumber + 4].replaceAll(",", "")));
+                            futures.setVolume(Integer.parseInt(trans[tranCodeNumber + 8].replaceAll(",", "")));
+                            futuresService.save(futures);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (tranCodeNumber == 0) {
+                        for (int c = tranCodeNumber; c < trans.length; c++) {
+                            if (trans[c] != null && trans[c].equals("品种月份")) {
+                                tranCodeNumber = c;
+                            }
+                        }
+                    }
+                }
+                i++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //读取excel
+    public static Workbook readExcel(String filePath) {
+        Workbook wb = null;
+        if (filePath == null) {
+            return null;
+        }
+        String extString = filePath.substring(filePath.lastIndexOf("."));
+        InputStream is = null;
+        try {
+            is = new FileInputStream(filePath);
+            if (".xls".equals(extString)) {
+                wb = new HSSFWorkbook(is);
+            } else if (".xlsx".equals(extString)) {
+                wb = new XSSFWorkbook(is);
+            } else {
+                wb = null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Sheet sheet = wb.getSheetAt(0);
+        int row_total = sheet.getPhysicalNumberOfRows();
+        int tranCodeNumber = 0;
+        for (int j = 0; j < row_total; j++) {
+            if (j <= 3) {
+                Row row = sheet.getRow(j);
+                if (tranCodeNumber != 0) {
+                    try {
+                        futures.setTrancode(row.getCell(tranCodeNumber).getRichStringCellValue().getString());
+                        futures.setTime(simpleDateFormat2.parse(row.getCell(tranCodeNumber - 1).getRichStringCellValue().getString()));
+                        futures.setOpening(Float.parseFloat(row.getCell(tranCodeNumber + 2).getRichStringCellValue().getString().replaceAll(",", "")));
+                        futures.setHighest(Float.parseFloat(row.getCell(tranCodeNumber + 3).getRichStringCellValue().getString().replaceAll(",", "")));
+                        futures.setLowest(Float.parseFloat(row.getCell(tranCodeNumber + 4).getRichStringCellValue().getString().replaceAll(",", "")));
+                        futures.setClosing(Float.parseFloat(row.getCell(tranCodeNumber + 5).getRichStringCellValue().getString().replaceAll(",", "")));
+                        futures.setVolume(Integer.parseInt(row.getCell(tranCodeNumber + 9).getRichStringCellValue().getString().replaceAll(",", "")));
+                        futuresService.save(futures);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (tranCodeNumber == 0) {
+                    for (int c = tranCodeNumber; c < row.getPhysicalNumberOfCells(); c++) {
+                        System.out.printf(row.getCell(c).getRichStringCellValue().getString());
+                        if (row.getCell(c).getCellTypeEnum() == STRING && row.getCell(c).getRichStringCellValue().getString().equals("品种代码")) {
+                            tranCodeNumber = c;
+                        }
+                    }
+                    System.out.println();
+                }
+
+                System.out.println("");
+            }
+        }
+
+        return wb;
+    }
+}
